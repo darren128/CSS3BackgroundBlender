f28c6d4becb5515adeb5ea4d1378c6b2d699846a
darren128 <dmrichards128@gmail.com>
2014-07-07 10:23:07 -0700

    Empty yo angular project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('bgblenderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
